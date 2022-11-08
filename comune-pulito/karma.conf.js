41c48781b8dcf3670026ad7b6070b0d2e6af0d1f
Galforno <ciccolellagiuseppe01@gmail.com>
2022-11-08 13:43:36 +0100

    Ricreazione Cartella

--- comune-pulito/karma.conf.js
+++ comune-pulito/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/codice-pulito'),
+      dir: require('path').join(__dirname, './coverage/comune-pulito'),
       subdir: '.',
       reporters: [
         { type: 'html' },
