07d7a96718df3f7cc67d8467d3a757750ce5318d
Galforno <ciccolellagiuseppe01@gmail.com>
2022-11-27 18:53:36 +0100

    Updated backend
    Aggiunta crittografia password e token
    Rifinitura codice

--- BackendSpringBoot/src/main/java/com/project/comunepulito/UserLoginController.java
+++ BackendSpringBoot/src/main/java/com/project/comunepulito/UserLoginController.java
@@ -1,6 +1,7 @@
 package com.project.comunepulito;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,41 +12,47 @@ import java.sql.Statement;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;  
+
+
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
 public class UserLoginController {
-	private String TABELLA;//TODO usare gli enum
+
 	private boolean successo;
+	private enum TABELLA {
+		utente,
+		gestore
+		}
 	
 	@GetMapping("/login")
 	public UserLogin userLogin(	@RequestParam(value="email",defaultValue="goku") String email,
 								@RequestParam(value="password",defaultValue="goku") String password,
 								@RequestParam(value="isGestore",defaultValue="false") boolean isGestore) {
-		TABELLA="utente";
+		TABELLA tab=TABELLA.utente;
 		if (isGestore){
-			this.TABELLA="gestore";
-		}
-		System.out.println(TABELLA);
-	    //password=generaToken(mail,pwd);  TODO: Implementare la crittografia del token
+			tab=TABELLA.gestore;
+		}		
+	    password=generaToken(email,password);
 		try{
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306","root","1234");
-			String query="SELECT *\nFROM comunepulito."+TABELLA+"\nWHERE email='"+email+"' AND pwd='"+password+"';";
+			String query="SELECT *\nFROM comunepulito."+tab+"\nWHERE email='"+email+"' AND pwd='"+password+"';";
 			Statement st = conn.createStatement();
 			ResultSet rs=st.executeQuery(query);
 			if (!rs.next()) {//caso in cui l'entità non è stata trovata nel db:autenticazione fallita
-				System.out.println("Credenziali non valide.");
 				successo=false;
+				password="";
 			} else {//è superfluo usare un ciclo, la query restituirà sempre solo un risultato.
-//					int id = rs.getInt("id_utente");
+//					int id = rs.getInt("id_utente");		Commentati per uso futuro
 //					String nome=rs.getString("nome");
 //					String cognome=rs.getString("cognome");
-					String mail=rs.getString("email");
-					String pwd=rs.getString("pwd");
+//					String mail=rs.getString("email");
+//					String pwd=rs.getString("pwd");
 //					int bilancio=rs.getInt("bilancio");
 //					int warn=rs.getInt("warn");
 //					int ban=rs.getInt("ban");
 					successo=true;
-				    password=generaToken(mail,pwd);
 			}
 			st.close();
 		}catch (Exception e){
@@ -56,7 +63,23 @@ public class UserLoginController {
 	}
 
 	static String generaToken(String mail, String pwd) {
-		// TODO crittografia di email+password
-		return pwd+mail;
+		String x="";
+		try {	
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");  //codifica dei token con algoritmo di hash SHA-256
+			byte[] hash = digest.digest((mail+pwd).getBytes(StandardCharsets.UTF_8));
+			StringBuilder hexString = new StringBuilder(2 * hash.length);
+		    for (int i = 0; i < hash.length; i++) {
+		        String hex = Integer.toHexString(0xff & hash[i]);
+		        if(hex.length() == 1) {
+		            hexString.append('0');
+		        }
+		        hexString.append(hex);
+		    }
+		    x=hexString.toString();
+		}catch(Exception e){
+			System.out.println("Errore durante la generazione del token"); //non accadrà mai, ma ok
+			e.printStackTrace();
+		}	
+		return x;
 	}
 }
--- BackendSpringBoot/src/main/java/com/project/comunepulito/UserSignupController.java
+++ BackendSpringBoot/src/main/java/com/project/comunepulito/UserSignupController.java
@@ -23,24 +23,22 @@ public class UserSignupController {
 			Statement st = conn.createStatement();
 			ResultSet rs=st.executeQuery(query);
 			if (!rs.next()) {//caso in cui l'entità non è stata trovata nel db:Utente da aggiungere.
-				query=String.format("INSERT INTO comunepulito.utente(nome,cognome,email,pwd,bilancio,warn,ban) VALUES(?,?,?,?,?,?,?);");
+				query="INSERT INTO comunepulito.utente(nome,cognome,email,pwd) VALUES(?,?,?,?);";
 				PreparedStatement psm =conn.prepareStatement(query);//Preparo la query
 				psm.setString(1, signupBody.getNome());
 				psm.setString(2, signupBody.getCognome());
 				psm.setString(3, signupBody.getEmail());
-				psm.setString(4, signupBody.getPassword());
-				psm.setInt(5, 0);
-				psm.setInt(6, 0);
-				psm.setInt(7, 0);
+				String password=UserLoginController.generaToken(signupBody.getEmail(),signupBody.getPassword());
+				psm.setString(4, password);
 				psm.execute();//Eseguo la query INSERT
 				conn.close();
-				return new SignupResponse(false,new UserLogin(true,UserLoginController.generaToken(signupBody.getEmail(),signupBody.getPassword())));//restituisco un feedback e il token di autenticazione
+				return new SignupResponse(false,new UserLogin(true,password));//restituisco un feedback e il token di autenticazione
 			}					
 		}catch (SQLIntegrityConstraintViolationException e) {
 			System.out.println("Email gia' utilizzata.");//Autoesplicativo, email è UNIQUE
 			return new SignupResponse(true,null);
 		}catch (Exception e) {
-			System.out.println("errore");
+			System.out.println("Errore");
 			e.printStackTrace();
 		}
 		return new SignupResponse(true,null);		
