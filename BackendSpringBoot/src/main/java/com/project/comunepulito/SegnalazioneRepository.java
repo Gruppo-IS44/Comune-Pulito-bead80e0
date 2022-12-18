e7516ed476e6f3480dc0707b8c78e6a893e8b7e1
PiovoloPolpette <117743371+AndreaDef@users.noreply.github.com>
2022-12-18 18:33:19 +0100

    update segnalazionerepository
    inizio script per la ricerca delle segnalazioni tramite id utente

--- BackendSpringBoot/src/main/java/com/project/comunepulito/SegnalazioneRepository.java
+++ BackendSpringBoot/src/main/java/com/project/comunepulito/SegnalazioneRepository.java
@@ -1,8 +1,13 @@
 package com.project.comunepulito;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface SegnalazioneRepository extends CrudRepository<Segnalazione,Integer>{
 	
-	Segnalazione findById_Utente(String Utente);
+	@Query("SELECT * FROM segnalazione WHERE Id_Utente = :Id_utente ")
+	Segnalazione findById_Utente(
+			@Param("Id_utente") String Utente);
+			
 }
