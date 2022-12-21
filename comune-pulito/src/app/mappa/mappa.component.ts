985f6dde9eef191054433b6f8ea4258e28c8dbfb
Hexygo <61022708+Hexygo@users.noreply.github.com>
2022-12-21 18:32:22 +0100

    updated mappa

--- comune-pulito/src/app/mappa/mappa.component.ts
+++ comune-pulito/src/app/mappa/mappa.component.ts
@@ -12,6 +12,7 @@ import GeoJSON from 'ol/format/GeoJSON';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import { Router } from '@angular/router';
+import { Point } from 'ol/geom';
 
 
 @Component({
@@ -40,7 +41,7 @@ export class MappaComponent implements OnInit {
         target: 'map',
         view: new View({
           constrainResolution: true,
-          center: fromLonLat([16.62662018, 49.2125578]), // starting position [lng, lat]
+          center: fromLonLat(this.getLocation().getCoordinates()), // starting position [lng, lat]
           zoom: 5 // starting zoom
         })
       });
@@ -59,6 +60,13 @@ export class MappaComponent implements OnInit {
           })
         }) 
       })
+      navigator.geolocation.getCurrentPosition((position)=>{
+        map.setView(new View({
+          constrainResolution:true,
+          center: fromLonLat([position.coords.longitude, position.coords.latitude]),
+          zoom:15
+        }))
+      })
       map.addLayer(AirportLayer);
   }
 
@@ -70,5 +78,16 @@ export class MappaComponent implements OnInit {
   clickReward(){
     console.log("stai andando alle reward")
     this.router.navigate(["/reward"])
+  }
+
+  getLocation():Point{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      const longitude = position.coords.longitude;
+        const latitude = position.coords.latitude;
+        console.log(longitude);
+        console.log(latitude);
+        return new Point([longitude, latitude]);
+    })
+    return new Point([10,10])
   }
 }
