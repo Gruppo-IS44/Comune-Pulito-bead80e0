ac4a2902a33cab7a8b18864cd533b13fd01a293f
Hexygo <61022708+Hexygo@users.noreply.github.com>
2022-12-22 11:33:21 +0100

    update

--- comune-pulito/src/app/segnalazione/segnalazione.component.ts
+++ comune-pulito/src/app/segnalazione/segnalazione.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Point from 'ol/geom/Point';
 
 @Component({
   selector: 'app-segnalazione',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./segnalazione.component.css']
 })
 export class SegnalazioneComponent {
+  posizione!:Point;
 
+  ngOnInit(){
+    this.getLocation();
+  }
+  
+  getLocation(): void{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+          const longitude = position.coords.longitude;
+          const latitude = position.coords.latitude;
+          console.log(longitude);
+          console.log(latitude);
+          this.posizione= new Point([longitude, latitude]);
+          console.log(this.posizione)
+        });
+    } else {
+       console.log("No support for geolocation")
+    }
+  }
 }
