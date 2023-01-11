acb28d63214cd6935596cee3c0969810ba7558ee
Hexygo <61022708+Hexygo@users.noreply.github.com>
2023-01-11 13:29:22 +0100

    Cluster Backend Final

--- BackendSpringBoot/src/main/java/com/project/comunepulito/RisolviClusterController.java
+++ BackendSpringBoot/src/main/java/com/project/comunepulito/RisolviClusterController.java
@@ -3,9 +3,7 @@ package com.project.comunepulito;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins="http://localhost:4200")
@@ -13,55 +11,38 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class RisolviClusterController {
 	@Autowired
 	private SegnalazioneRepository segnalazioneRepository;
+	@Autowired
+	private ClusterRepository clusterRepository;
+	@Autowired
+	private UtenteRepository utenteRepository;
 	
 	@PostMapping("/convalida")
 	public Integer Esempio (@RequestBody RisolviClusterBody risolviclusterBody){
-		
-//		Segnalazione s = segnalazioneRepository.findById(risolviclusterBody.getSegnalazione()).get();
-//		
-//		
-//		s.setId_Stato(risolviclusterBody.getId_Stato());
-//		System.out.println(risolviclusterBody.getId_Stato());
-//		segnalazioneRepository.save(s);
 		System.out.println(risolviclusterBody);
-		
-/*		Segnalazione s = segnalazioneRepository.findById(risolviclusterBody.getSegnalazione()).get();
-		
-		Utente u = utenteRepository.findById(risolviclusterBody.getId_Utente()).get();
-		
-		s.setId_Stato(risolviclusterBody.getId_Stato());
-
-		if (s.getId_Stato() == 3) {		// 3 è la segnalazione NON VALIDA
-			
-			System.out.println("Segnalazione NON-VALIDA");
+		for(Segnalazione s:risolviclusterBody.getSegnalazioni()) {
+			segnalazioneRepository.save(s);
+			Utente u=utenteRepository.findById(s.getId_utente()).get();
+			if(s.getId_Stato()==2) {//Segnalazione Valida
+				System.out.println(s.getId_Tipo());
+				switch(s.getId_Tipo()) {
+					case 1:
+						u.setBilancio(u.getBilancio()+10);// 1 RIFIUTO PICCOLO
+						break;
+					case 2:
+						u.setBilancio(u.getBilancio()+20);// 2 RIFIUTO GRANDE
+						break;
+					case 3:
+						u.setBilancio(u.getBilancio()+30);// 3 RIFIUTO ALTRO
+						break;
+				}
+				continue;
+			}//Segnalazione non valida=>Warn Utente
 			u.setWarn(u.getWarn()+1);
-			
-			if(u.getWarn() == 3) {
+			if(u.getWarn()==3)//Al terzo warn, l'utente viene bannato.
 				u.setBan(true);
-			}
-
+			utenteRepository.save(u);
 		}
-		
-		System.out.println(risolviclusterBody.getId_Stato());
-		segnalazioneRepository.save(s);
-		
-		switch(s.getId_Tipo()) {
-			case 1:
-				u.setBilancio(u.getBilancio()+10);		// 1 RIFIUTO PICCOLO
-				break;
-			case 2:
-				u.setBilancio(u.getBilancio()+20);		// 2 RIFIUTO GRANDE
-				break;
-			case 3:
-				u.setBilancio(u.getBilancio()+30);		// 3 RIFIUTO ALTRO
-				break;			
-
-		}
-				System.out.println("Bilancio utente aumentato: " + u.getBilancio());	*/
-
+		clusterRepository.save(risolviclusterBody.getCluster());
 		return null;
 	}
-	
-	
-
 }
